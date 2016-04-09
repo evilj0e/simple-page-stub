a9106d62aa011ed8aa47388959085f02f660f91d
Anton Konev <evilj0e@yandex-team.ru>
2016-04-09 23:04:25 +0500

    ISSUE-1: Use stylelint

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + "/build"));
 
-app.get('/', function (request, response) {
-    response.sendFile(__dirname + '/build/index.html');
+app.get("/", function (request, response) {
+    response.sendFile(__dirname + "/build/index.html");
 });
 
 app.listen(port, function () {
