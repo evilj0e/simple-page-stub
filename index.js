73e13e9378a259011e89cfad3f1879ca4eb7691e
Anton Konev <evilj0e@yandex-team.ru>
2016-04-09 22:25:45 +0500

    Fix yml

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var express = require("express");
 var app = express();
+var port = process.env.PORT || 5000;
+
 app.use(express.static(__dirname + "/build"));
 
 app.get('/', function (request, response) {
     response.sendFile(__dirname + '/build/index.html');
 });
 
-var port = process.env.PORT || 5000;
 app.listen(port, function () {
     console.log("Listening on " + port);
 });
